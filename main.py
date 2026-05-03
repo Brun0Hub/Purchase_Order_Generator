bca63904924c3c0bac32be6f8b5f8d5c1abc48ce
Bruno Santos <brunordss25@gmail.com>
2026-05-03 18:38:04 -0300

    main.py adding the Outlook function

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from functions import generate_orders_batch
+from functions import send_email
 
 if __name__ == "__main__":
     generate_orders_batch("pedidos.csv")
+
+    send_email(
+    to="bruno_rdss@hotmail.com",
+    cc="rafake_9@hotmail.com;bruno.-.santos@hotmail.com",
+    bcc="bruno.-.santos@hotmail.com",
+    subject="Order Request",
+    html_body="""<p>Dear all,</p>
+                 <p>Please note it attached.</p>
+                 <img src='seu_link_imagem' wdth=200>"""
+)
